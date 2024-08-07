a9364df05ceba38448605f239be7abe8850279b2
MukitHossen7 <hossenmukit7@gmail.com>
2024-08-07 08:30:53 +0600

    ready to my project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0092E4",
+        secondary: "#0F2137",
+        Tertiary: "#183656",
+      },
+    },
+  },
+  plugins: [],
+};
